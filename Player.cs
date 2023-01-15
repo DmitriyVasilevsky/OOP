2864592ba35604aebfb12363469b4205a65f7a49
DmitriyVasilevsky <vasilek_1234@mail.ru>
2023-01-15 15:17:44 +0300

    Improve code style.

--- Player.cs
+++ Player.cs
@@ -2,37 +2,14 @@
 {
     internal class Player
     {
-        private int _positionX;
-        private int _positionY;
+        public int PositionX { get; private set; }
 
-        public int PositionX
-        {
-            get
-            {
-                return _positionX;
-            }
-            private set
-            {
-                _positionX = value;
-            }
-        }
+        public int PositionY { get; private set; }
 
-        public int PositionY
+        public Player(int positionX, int positionY)
         {
-            get
-            {
-                return _positionY;
-            }
-            private set
-            {
-                _positionY = value;
-            }
-        }
-
-        public Player(int PositionX, int PositionY)
-        {
-            _positionX = PositionX;
-            _positionY = PositionY;
+            PositionX = positionX;
+            PositionY = positionY;
         }
     }
 }
