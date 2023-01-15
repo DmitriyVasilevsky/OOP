ba1afcd4f1e755875db366b9a4f58b0a900625f9
DmitriyVasilevsky <vasilek_1234@mail.ru>
2023-01-15 15:08:53 +0300

    Add working version.

---
+++ Player.cs
@@ -0,0 +1,38 @@
+﻿namespace OOP
+{
+    internal class Player
+    {
+        private int _positionX;
+        private int _positionY;
+
+        public int PositionX
+        {
+            get
+            {
+                return _positionX;
+            }
+            private set
+            {
+                _positionX = value;
+            }
+        }
+
+        public int PositionY
+        {
+            get
+            {
+                return _positionY;
+            }
+            private set
+            {
+                _positionY = value;
+            }
+        }
+
+        public Player(int PositionX, int PositionY)
+        {
+            _positionX = PositionX;
+            _positionY = PositionY;
+        }
+    }
+}
---
+++ Renderer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace OOP
+{
+    internal class Renderer
+    {
+        public void DrawPlayer(int PositionX, int PositionY, char player = '@')
+        {
+            Console.SetCursorPosition(PositionX, PositionY);
+            Console.Write(player);
+
+        }
+    }
+}
