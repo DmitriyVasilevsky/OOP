65cdab11193fb8a020d5f1af1f2e89fcadb61441
DmitriyVasilevsky <vasilek_1234@mail.ru>
2023-01-15 14:57:53 +0300

    Add player.

---
+++ Player.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace OOP
+{
+    internal class Player
+    {
+        private string _name;
+        private int _strength;
+        private int _agility;
+        private int _level;
+
+        public Player(string name, int strength, int agility, int level)
+        {
+            _name = name;
+            _strength = strength;
+            _agility = agility;
+            _level = level;
+        }
+
+        public void ShowStats()
+        {
+            Console.WriteLine("Характеристики игрока " + _name + ":\n" +
+                              "Сила - " + _strength + ";\n" +
+                              "Ловкость - " + _agility + ";\n" +
+                              "Уровень - " + _level + ";\n");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿namespace OOP
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Player[] players = { new Player("Alex", 15, 20, 3), new Player("John", 10, 10, 1) };
+
+            for (int i = 0; i < players.Length; i++)
+            {
+                players[i].ShowStats();
+            }
+        }
+    }
+}
